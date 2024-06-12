5315fa09c72a6fa93ba00880acab41a93a397ced
ajinata84 <ajinata89@gmail.com>
2024-06-12 18:39:37 +0700

    prod url

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -2,7 +2,7 @@ export function getApiURL () {
     const prodUrl = "https://api-typink.djie.cloud"
     const devUrl = "http://localhost:7000"
 
-    return devUrl
+    return prodUrl
 }
 
 export const genreMap = [
